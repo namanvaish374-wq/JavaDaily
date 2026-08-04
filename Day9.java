212991e3876add9bc78a0b35f24864f6912a325c
Naman Vaish <namanvaish374@gmail.com>
2026-08-04 20:58:00 +0530

    Day-9

---
+++ Day9.java
@@ -0,0 +1,31 @@
+/*Cab Rides
+Chef and his family is trying to reach the airport. Including Chef, they are a total of 
+N
+N members.
+
+A single cab ride costs 
+max
+⁡
+(
+200
+,
+100
+⋅
+X
+)
+max(200,100⋅X) rupees to transport 
+X
+X people from Chef's home to th*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Day9
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+	}
+}
