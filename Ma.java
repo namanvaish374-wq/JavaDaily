4b814c67e0820be5c6f492dcaa9cd6ca4de29b81
Naman Vaish <namanvaish374@gmail.com>
2026-08-15 23:46:47 +0530

    DAY 19

---
+++ Ma.java
@@ -0,0 +1,91 @@
+/*
+ Chef and Dolls
+ 
+
+Chef is fan of pairs and he likes all things that come in pairs. He even has a doll collection in which the dolls come in pairs. One day while going through his collection he found that there are odd number of dolls. Someone had stolen a doll!!!
+
+Help chef find which type of doll is missing..*/
+
+import java.util.*;
+
+class Mai{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            int ans = 0;
+
+            for (int i = 0; i < n; i++) {
+                ans ^= sc.nextInt();
+            }
+
+            System.out.println(ans);
+        }
+    }
+}
+
+
+/*
+CodeChef Streak
+CodeChef offers a feature called streak count. A streak is maintained if you solve at least one problem daily.
+
+Om and Addy actively maintain their streaks on CodeChef. Over a span of 
+N
+N consecutive days, you have observed the count of problems solved by each of them.
+
+Your task is to determine the maximum streak achieved by Om and Addy and find who had the longer maximum streak. */
+
+
+public class Ma{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t--> 0) {
+            int n = scanner.nextInt();
+            int[] om = new int[n];
+            int[] addy = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                om[i] = scanner.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                addy[i] = scanner.nextInt();
+            }
+
+            // Your code goes here
+            int omCurr = 0, omMax = 0;
+            for (int i = 0; i < n; i++) {
+                if (om[i] > 0) {
+                    omCurr++;
+                    omMax = Math.max(omMax, omCurr);
+                } else {
+                    omCurr = 0;
+                }
+            }
+
+            int addyCurr = 0, addyMax = 0;
+            for (int i = 0; i < n; i++) {
+                if (addy[i] > 0) {
+                    addyCurr++;
+                    addyMax = Math.max(addyMax, addyCurr);
+                } else {
+                    addyCurr = 0;
+                }
+            }
+
+            if (omMax > addyMax) {
+                System.out.println("Om");
+            } else if (addyMax > omMax) {
+                System.out.println("Addy");
+            } else {
+                System.out.println("Draw");
+            }
+        }
+    }
+}
