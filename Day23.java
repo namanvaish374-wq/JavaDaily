3aa399d8a2eba727b363e61a01d3d49e2969969d
Naman Vaish <namanvaish374@gmail.com>
2026-08-19 11:09:03 +0530

    DAY 23

---
+++ Day23.java
@@ -0,0 +1,33 @@
+
+//ARRAYLIST LEARNING 
+import java.util.*;
+
+class Main {
+
+    public static void main(String args[]) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+
+        System.out.println(list.get(1));
+
+        list.set(1,100);
+
+        System.out.println(list);
+
+        list.remove(0);
+
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        System.out.println(list.contains(30));
+
+    }
+
+}
