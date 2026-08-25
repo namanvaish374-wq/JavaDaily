bc0394afc5943a95895e97c0ce26c643cd9b63d9
Naman Vaish <namanvaish374@gmail.com>
2026-08-25 21:46:23 +0530

    Day 27

--- Day26.java
+++ Day26.java
@@ -1,5 +1,49 @@
-package JavaDaily;
+// import java.util.*;
+// class Student {
 
-public class Day26 {
-    HELLO i am naman vai9sh 
+//     String name;
+//     int marks;
+
+//     Student(String name, int marks) {
+//         this.name = name;
+//         this.marks = marks;
+//     }
+
+//     void display() {
+//         System.out.println(name + " " + marks);
+//     }
+//    public static void main(String[] args) {
+//     ArrayList<Student> students=new ArrayList<>();
+//     students.add(new Student("Naman",95));
+//     students.add(new Student("Rahul",88));
+//     students.add(new Student("Naitik",76));
+
+//     for(int i=0;i<students.size();i++){
+//         students.get(i).display();
+//     }
+
+//     for(Student s : students){
+//     s.display();
+//     }
+    
+//    }
+// }
+
+class Employee{
+    int id;
+    String name;
+    double salary;
+
+    Employee(int id , String name, double salary){
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+    public void display(){
+        System.out.println(id+" "+name+" "+salary);
+    }
+    public static void main(String[] args) {
+        
+    }
 }
+
