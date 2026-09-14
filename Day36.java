6cc525dfdab1898bf788b7aca318f57501c3b68d
Naman Vaish <namanvaish374@gmail.com>
2026-09-14 23:45:07 +0530

    Day37

---
+++ Day36.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Day36
+{
+    public static void main(String[] args) {
+        int arr[]=new int[10];
+        Scanner sc=new Scanner(System.in);
+        HashSet<Integer> arr1=new HashSet<>();
+        for (int i=0; i<10;i++){
+           arr[i]=new Array();
+
+           ,sxjsmdloeicddlc;d;
+           
+        }
+    }
+}
